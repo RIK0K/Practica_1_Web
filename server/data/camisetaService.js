b3d74e3858532259d910d73a788686dada07c9a4
RIKOK <a.rico7104@gmail.com>
2023-12-08 12:31:33 +0100

    obtener elemento en el server, y productosService

---
+++ server/data/camisetaService.js
@@ -0,0 +1,26 @@
+export const camisetas= [
+    {
+        "id": 1,
+        "nombre": "Camiseta Blanca",
+        "precio": 25,
+        "imagen": "ruta/de/la/imagen1.jpg",
+        "descripcion": "Descripción de la camiseta blanca.",
+        "categoria": "camisetas"
+      },
+      {
+        "id": 2,
+        "nombre": "Camiseta Blanca",
+        "precio": 25,
+        "imagen": "ruta/de/la/imagen1.jpg",
+        "descripcion": "Descripción de la camiseta blanca.",
+        "categoria": "camisetas"
+      },
+      {
+        "id": 3,
+        "nombre": "Camiseta Blanca",
+        "precio": 25,
+        "imagen": "ruta/de/la/imagen1.jpg",
+        "descripcion": "Descripción de la camiseta blanca.",
+        "categoria": "camisetas"
+      }
+]
---
+++ server/data/sudaderaService.js
@@ -0,0 +1,26 @@
+export const sudaderas= [
+    {
+        "id": 1,
+        "nombre": "Camiseta Blanca",
+        "precio": 25,
+        "imagen": "ruta/de/la/imagen1.jpg",
+        "descripcion": "Descripción de la camiseta blanca.",
+        "categoria": "sudaderas"
+      },
+      {
+        "id": 2,
+        "nombre": "Camiseta Blanca",
+        "precio": 25,
+        "imagen": "ruta/de/la/imagen1.jpg",
+        "descripcion": "Descripción de la camiseta blanca.",
+        "categoria": "sudaderas"
+      },
+      {
+        "id": 3,
+        "nombre": "Camiseta Blanca",
+        "precio": 25,
+        "imagen": "ruta/de/la/imagen1.jpg",
+        "descripcion": "Descripción de la camiseta blanca.",
+        "categoria": "sudaderas"
+      }
+]
