886ed815ad8a082f0b3fcc6fbe86a8586ad115ce
RIKOK <a.rico7104@gmail.com>
2023-12-10 20:08:34 +0100

    caragar en un array los diferentes productos

--- server/server/app.js
+++ server/server/app.js
@@ -1,12 +1,45 @@
-
 import express from 'express';
 import cors from 'cors';
 const app = express();
 const port = 3000;
 
+
 import {
   obtenerElemento, obtenerElementos, guardarElemento, crearElemento, borrarElemento, obtenerResenasDeLosElementos, crearResenaDelElemento, obtenerResenaDelElemento, iniciarArchivosDeElementos
 } from "./metodos/metodos.js";
+
+
+// Definir el array elementos
+let elementos = [];
+
+// Función para cargar un archivo JSON
+async function cargarDatos(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error al cargar el archivo JSON:', error);
+    return null;
+  }
+}
+
+// Cargar los datos de los archivos JSON y combinarlos en el array elementos
+Promise.all([
+  cargarDatos('data/sudaderasService.json'),
+  cargarDatos('data/camisetasService.json'),
+  cargarDatos('data/gorrosService.json')
+])
+  .then((resultados) => {
+    // Combina los arrays en un solo array elementos
+    elementos = elementos.concat(resultados.map((resultado) => Object.values(resultado)[0]));
+
+    // Imprime el array elementos
+    console.log('Array elementos:', elementos);
+  })
+  .catch((error) => {
+    console.error('Error al cargar los archivos JSON:', error);
+  });
 
 
 app.init
