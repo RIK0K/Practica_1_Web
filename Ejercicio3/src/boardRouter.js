4dae25244220390d80b59603a14153fe9e992203
RIKOK <a.rico7104@gmail.com>
2023-12-11 23:14:31 +0100

    comentar practicas

--- Ejercicio3/src/boardRouter.js
+++ Ejercicio3/src/boardRouter.js
@@ -1,66 +1,80 @@
+// Importar el módulo 'express'
 import express from 'express';
+
+// Importar funciones del servicio de tablero
 import * as boardService from './boardService.js';
 
+// Crear una instancia del enrutador de Express
 const router = express.Router();
 
+// Ruta para manejar la solicitud GET a '/'
 router.get('/', (_req, res) => {
+    // Obtener publicaciones de diferentes categorías del servicio de tablero
     const sudaderas = boardService.getPosts('sudaderas');
     const camisetas = boardService.getPosts('camisetas');
     const gorros = boardService.getPosts('gorros');
 
+    // Renderizar la vista 'index' con las publicaciones obtenidas
     res.render('index', { sudaderas, camisetas, gorros });
 });
 
-
+// Ruta para manejar la solicitud POST a '/post/new'
 router.post('/post/new', (req, res) => {
+    // Obtener datos del cuerpo de la solicitud
     let { nombre, descripcion, precio, elementRadio, img } = req.body;
+    // Agregar una nueva publicación al servicio de tablero
     boardService.addPost(elementRadio, { nombre, descripcion, precio, img });
+    // Renderizar la vista 'saved_post'
     res.render('saved_post');
 });
 
+// Ruta para manejar la solicitud GET a '/post/:id'
 router.get('/post/:id', (req, res) => {
-
+    // Obtener la publicación según el ID proporcionado
     let post = boardService.getPost(req.params.id);
-
+    // Renderizar la vista 'show_post' con la publicación obtenida
     res.render('show_post', { post });
 });
 
+// Ruta para manejar la solicitud GET a '/post/:id/delete'
 router.get('/post/:id/delete', (req, res) => {
-
+    // Eliminar la publicación según el ID proporcionado
     boardService.deletePost(req.params.id);
-
+    // Renderizar la vista 'deleted_post'
     res.render('deleted_post');
 });
 
-
-
+// Ruta adicional que maneja la solicitud GET a '/'
 router.get('/', (req, res) => {
-    res.render('Producto',{
+    // Renderizar la vista 'Producto' con datos del cuerpo de la solicitud
+    res.render('Producto', {
         name: req.body.name,
         rating: req.body.rating,
         review: req.body.review,
     });
 });
 
+// Arreglo para almacenar reseñas
 let reviews = [];
 
-
+// Ruta para manejar la solicitud POST a '/:review'
 router.post('/:review', (req, res) => {
-    // Add the new review to the array
+    // Agregar la nueva reseña al arreglo
     if (!req.body.name || !req.body.rating || !req.body.review) {
+        // Renderizar la vista 'Producto' con un mensaje de error si faltan campos
         res.render('Producto', { errorMessage: 'Por favor ingrese todos los campos', reviews: reviews });
         return;
-    }
-    else{
-    reviews.push({
-        name: req.body.name,
-        rating: req.body.rating,
-        review: req.body.review
-    });
+    } else {
+        reviews.push({
+            name: req.body.name,
+            rating: req.body.rating,
+            review: req.body.review
+        });
 
-    // Render the 'Producto' page with the updated reviews array
-    res.render('Producto', { reviews: reviews });
-}
+        // Renderizar la vista 'Producto' con el arreglo de reseñas actualizado
+        res.render('Producto', { reviews: reviews });
+    }
 });
 
+// Exportar el enrutador para su uso en otras partes de la aplicación
 export default router;
--- Ejercicio3/src/boardService.js
+++ Ejercicio3/src/boardService.js
@@ -1,60 +1,64 @@
+// Objeto 'posts' que almacena categorías (camisetas, sudaderas, gorros), cada una representada por un mapa.
 const posts = {
     camisetas: new Map(),
     sudaderas: new Map(),
     gorros: new Map(),
 };
 
+// Variable para asignar identificadores únicos a cada post.
 let nextId = 0;
 
+// Función para agregar un post a la categoría especificada.
 export const addPost = (elementRadio, post) => {
-    // Verifica si posts[category] existe y es un objeto Map
+    // Si la categoría no existe o no es un objeto tipo 'Map', se crea un nuevo mapa.
     if (!posts[elementRadio] || !(posts[elementRadio] instanceof Map)) {
-        // Inicializa posts[category] como un nuevo objeto Map
         posts[elementRadio] = new Map();
     }
 
+    // Se asigna un nuevo identificador al post y se agrega al mapa correspondiente.
     let id = nextId++;
     post.id = id.toString();
     posts[elementRadio].set(post.id, post);
 }
 
-
-export const deletePostost = (elementRadio,id) => {
+// Función para eliminar un post de una categoría específica.
+export const deletePostost = (elementRadio, id) => {
+    // Elimina el post con el ID especificado de la categoría dada.
     posts[elementRadio].delete(id);
 }
 
-
-
+// Función para obtener todos los posts de una categoría.
 export function getPosts(elementRadio) {
+    // Devuelve un array con todos los posts de la categoría especificada.
     return [...posts[elementRadio].values()];
 }
 
-
+// Función para obtener un post específico de una categoría.
 export const getPost = (elementRadio, id) => {
     if (posts[elementRadio]) {
+        // Devuelve el post con el ID dado de la categoría especificada.
         return posts[elementRadio].get(id);
     } else {
-       
+        // Si la categoría no existe, muestra un mensaje de error en la consola.
         console.error(`La categoría '${elementRadio}' no existe.`);
-        return null; // O algo que indique que no se encontró el post
+        return null;
     }
 }
 
+// Ejemplos de uso: se añaden varios posts a las categorías 'sudaderas', 'camisetas' y 'gorros'.
+addPost("sudaderas", { img: "https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/293a76d0f5e97b6853ee64e6af02875d.jpg?imageView2/2/w/800/q/70" });
+addPost("sudaderas", { img: "https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/79cfe040ec56194cb973b68c20e12418.jpg?imageView2/2/w/800/q/70" });
+addPost("sudaderas", { img: "https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/d259fefb123d2776e72797e67eec8606.jpg?imageView2/2/w/800/q/70" });
+addPost("sudaderas", { img: "https://img.kwcdn.com/product/open/2023-11-28/1701176876994-7519af7b53d7417f9214159b794def69-goods.jpeg?imageView2/2/w/800/q/70" });
 
-addPost("sudaderas", { img:"https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/293a76d0f5e97b6853ee64e6af02875d.jpg?imageView2/2/w/800/q/70" });
-addPost("sudaderas", { img:"https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/79cfe040ec56194cb973b68c20e12418.jpg?imageView2/2/w/800/q/70" });
-addPost("sudaderas", { img:"https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/d259fefb123d2776e72797e67eec8606.jpg?imageView2/2/w/800/q/70"});
-addPost("sudaderas", { img:"https://img.kwcdn.com/product/open/2023-11-28/1701176876994-7519af7b53d7417f9214159b794def69-goods.jpeg?imageView2/2/w/800/q/70"});
+addPost("camisetas", { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcL1d8maC63fJB4XSMzDQw7oeWJplE4OacnWmP4_ZvM3LmRIM9l_HG32XsrBP7-cNdBBg&usqp=CAU" });
+addPost("camisetas", { img: "https://m.media-amazon.com/images/I/617ysECwjML._AC_SX679_.jpg" });
+addPost("camisetas", { img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.redbubble.com%2Fes%2Fshop%2Fel%2Bave%2Bfenix%2Bwomens-clothes&psig=AOvVaw2Der-w1-ZTyYsB3M52fZR0&ust=1702416406066000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMiN2rqpiIMDFQAAAAAdAAAAABAS" });
+addPost("camisetas", { img: "https://lovecamisetas.com/wp-content/uploads/2021/07/camiseta-negra-ave-fenix-ok.jpg" });
+addPost("camisetas", { img: "https://image.spreadshirtmedia.net/image-server/v1/products/T6A5PA5835PT17X29Y28D160103111W22583H42310/views/1,width=120,height=120,appearanceId=5,backgroundColor=F2F2F2,modelId=1081,crop=detail/fenix-negro-camiseta-hombre.jpg" });
 
-
-addPost("camisetas", { img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcL1d8maC63fJB4XSMzDQw7oeWJplE4OacnWmP4_ZvM3LmRIM9l_HG32XsrBP7-cNdBBg&usqp=CAU"  });
-addPost("camisetas", { img:"https://m.media-amazon.com/images/I/617ysECwjML._AC_SX679_.jpg"  });
-addPost("camisetas", { img:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.redbubble.com%2Fes%2Fshop%2Fel%2Bave%2Bfenix%2Bwomens-clothes&psig=AOvVaw2Der-w1-ZTyYsB3M52fZR0&ust=1702416406066000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMiN2rqpiIMDFQAAAAAdAAAAABAS" });
-addPost("camisetas", { img:"https://lovecamisetas.com/wp-content/uploads/2021/07/camiseta-negra-ave-fenix-ok.jpg" });
-addPost("camisetas", { img:"https://image.spreadshirtmedia.net/image-server/v1/products/T6A5PA5835PT17X29Y28D160103111W22583H42310/views/1,width=120,height=120,appearanceId=5,backgroundColor=F2F2F2,modelId=1081,crop=detail/fenix-negro-camiseta-hombre.jpg" });
-
-addPost("gorros", { img:"https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/828d4f64b673f39f3dcf8cad3bf993f7.jpg?imageView2/2/w/800/q/70" });
-addPost("gorros", { img:"https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/f1f6d87584829d4041f7839f0da74dbb.jpg?imageView2/2/w/800/q/70" });
-addPost("gorros", { img:"https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/64bff496d61e159c93f6e6151ec26964.jpg?imageView2/2/w/800/q/70"  });
-addPost("gorros", { img:"https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/004f02538893c518f6bdff0e385ce24e.jpg?imageView2/2/w/800/q/70" });
-addPost("gorros", { img:"https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/eb11dcd38d0089838cfad5d0cbb35987.jpg?imageView2/2/w/800/q/70" });
+addPost("gorros", { img: "https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/828d4f64b673f39f3dcf8cad3bf993f7.jpg?imageView2/2/w/800/q/70" });
+addPost("gorros", { img: "https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/f1f6d87584829d4041f7839f0da74dbb.jpg?imageView2/2/w/800/q/70" });
+addPost("gorros", { img: "https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/64bff496d61e159c93f6e6151ec26964.jpg?imageView2/2/w/800/q/70" });
+addPost("gorros", { img: "https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/004f02538893c518f6bdff0e385ce24e.jpg?imageView2/2/w/800/q/70" });
+addPost("gorros", { img: "https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/eb11dcd38d0089838cfad5d0cbb35987.jpg?imageView2/2/w/800/q/70" });
