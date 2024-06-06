e4959dbfda73493582fd618b34347509f834b48c
GaizkaURJC <g.aranbarri.2022@alumnos.urjc.es>
2024-06-06 16:48:25 +0200

    Validacion de formulario añadir nuevo producto

--- Practica_3/public/functions.js
+++ Practica_3/public/functions.js
@@ -29,74 +29,79 @@ function ejecutarTema() {
 
 
 // Comprobacion de datos del formulario
-const formulario = document.getElementById('formulario');
-const inputs = document.querySelectorAll('#formulario input');
+function formValidation(){
+    const formulario = document.getElementById('formulario');
+    const inputs = document.querySelectorAll('#formulario input, #formulario textarea');
 
-const expresiones = {
-    nombre: /^[a-zA-ZÀ-ÿ\s]{1,100}$/,
-    imagen: /https?:\/\/[\w\-\.]+\.\w{2,5}\/?\S*/,
-    precio: /^.{1,3}$/
-}
-
-const campos = {
-    nombre: false,
-    imagen: false,
-    precio: false,
-}
-
-const validarFormulario = (e) => {
-    switch (e.target.name) {
-        case "nombre":
-            validarCampo(expresiones.nombre, e.target, 'nombre');
-            break;
-        case "img":
-            validarCampo(expresiones.imagen, e.target, 'imagen');
-            break;
-        case "precio":
-            validarCampo(expresiones.precio, e.target, 'precio');
-            break;
+    const expresiones = {
+        nombre: /^[A-ZÀ-ÿ][a-zA-ZÀ-ÿ\s]{0,99}$/,
+        imagen: /https?:\/\/[\w\-\.]+\/[\w\-\.\/]+.(jpg|jpeg|png|gif|bmp|svg)\b|data:image\/[a-zA-Z]+;base64,[\w=+\/]+|img\.brave\.[\w\-\.]+/,        precio: /^[0-9]{1,3}(?:\.[0-9]{1,2})?$/,
+        descripcion: /^.{50,500}$/,
+        precio: /^(?:[1-9][0-9]{0,2}(?:\.[0-9]{1,2})?|1\.[0-9]{1,2})$/
     }
-}
-
-const validarCampo = (expresion, input, campo) => {
-    if (expresion.test(input.value)) {
-        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
-        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
-        document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
-        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
-        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
-        campos[campo] = true;
-    } else {
-        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
-        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-        document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
-        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
-        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
-        campos[campo] = false;
+    const campos = {
+        nombre: false,
+        imagen: false,
+        precio: false,
+        descripcion: false
     }
-}
 
-inputs.forEach((input) => {
-    input.addEventListener('keyup', validarFormulario);
-    input.addEventListener('blur', validarFormulario);
-});
-
-formulario.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const terminos = document.getElementById('terminos');
-    if (campos.nombre && campos.imagen && campos.precio) {
-        formulario.reset();
-
-        document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
-        setTimeout(() => {
-            document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
-        }, 5000);
-
-        document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
-            icono.classList.remove('formulario__grupo-correcto');
-        });
-    } else {
-        document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+    const validarFormulario = (e) => {
+        switch (e.target.name) {
+            case "nombre":
+                validarCampo(expresiones.nombre, e.target, 'nombre');
+                break;
+            case "img":
+                validarCampo(expresiones.imagen, e.target, 'imagen');
+                break;
+            case "precio":
+                validarCampo(expresiones.precio, e.target, 'precio');
+                break;
+            case "descripcion":
+                validarCampo(expresiones.descripcion, e.target, 'descripcion');
+                break;
+        }
     }
-});
+
+    const validarCampo = (expresion, input, campo) => {
+        if (expresion.test(input.value)) {
+            document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+            document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+            document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+            document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+            document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+            campos[campo] = true;
+        } else {
+            document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+            document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+            document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+            document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+            document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+            campos[campo] = false;
+        }
+    }
+
+    inputs.forEach((input) => {
+        input.addEventListener('keyup', validarFormulario);
+        input.addEventListener('blur', validarFormulario);
+    });
+
+    formulario.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        if (campos.nombre && campos.imagen && campos.precio) {
+            formulario.reset();
+
+            document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+            setTimeout(() => {
+                document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+            }, 5000);
+
+            document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+                icono.classList.remove('formulario__grupo-correcto');
+            });
+        } else {
+            document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+        }
+    });
+}
